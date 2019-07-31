5988909e4998110e35290b99a26d0640d93c85de
dedane <evanskeema@gmail.com>
2019-07-31 13:33:22 +0300

    first commit

---
+++ components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="header">
+     <div class="logo">
+       <img src="*~/assets/img/javascript.png"/>
+     </div>
+     <div class="nav-items">
+       <i class="material-icons">perm_identity</i>
+       <i class="material-icons">lock</i>
+       <i class="material-icons">lock_open</i>
+       <i class="material-icons">mail_outline</i>
+     </div>
+
+</div>
+
+
+
+</template>
+
+ <style >
+      .logo{
+          float: left;
+          position: relative;
+          margin: 10px 15px 15px 10px;
+          }
+      .nav-items{
+          display: inline-block;
+          vertical-align: top;
+          width: 50px;
+          height: 50px;
+          margin-right: 20px;
+          margin-top: 15px; 
+      }
+ </style>
+
+ 
---
+++ layouts/Navigations/nav.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="id">
+          <nav class="nav justify-content-center">
+          <a class="nav-link active" href="#">Active link</a>
+          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link disabled" href="#">Disabled link</a>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
